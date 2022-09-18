8d3008e7c193812b5e8f6c210069e5d58f6b0ca9
Garima <raigarima.id@gmail.com>
2022-09-18 10:50:05 +0530

    mini projects

---
+++ spotify_project/script.js
@@ -0,0 +1,13 @@
+console.log("Welcome to HEAR ME OUT ");
+let songIndex = 0;
+let audioElement = new Audio('mp3.mp3');
+let masterPlay = document.getElementById()
+let songs = [
+    {songName: "Salam-e-Ishq", filePath:"song/mp3.mp3", coverPath:"covers/cover1.jpg"}
+]
+
+//audioElement.play();
+
+// Listen to Events
+document.addEventListener('time')
+
